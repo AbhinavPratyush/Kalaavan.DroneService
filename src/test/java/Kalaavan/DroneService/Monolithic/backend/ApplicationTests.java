49d0a20c69f88f464c3a6d9feabb285e3168bb34
AbhinavPratyush <abhinavpratyush273@gmail.com>
2026-05-30 09:21:50 +0530

    Started the project

---
+++ src/test/java/Kalaavan/DroneService/Monolithic/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Kalaavan.DroneService.Monolithic.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
